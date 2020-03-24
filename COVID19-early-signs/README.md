4ff6d8270b6c72e16f63d1e4d9a92655c7c3325e
ryankruse <ryan.kruse9@gmail.com>
2020-03-24 00:23:34 -0500

    add COVID19-early-signs

---
+++ COVID19-early-signs/README.md
@@ -0,0 +1,49 @@
+# COVID19
+
+<h2>COVID-19: Early Signs</h2>
+ A project for analyzing the likelihood of coronavirus presence in a state prior to the date of the first confirmed case. Project details below.
+
+<h3>File description:</h3>
+
+**states-forecasts-95-pi.pdf** - Has plots of all states in our dataset. Plots show forecasted **rILI-** using a 95% prediction interval, observed data, and the date of the state's first confirmed case of COVID-19. Does not include Florida, New Jersey, or Rhode Island due to lack of data.
+
+**states-forecast-99-pi.pdf** - Has plots of all states in our dataset. Plots show forecasted **rILI-** using a 99% prediction interval, observed data, and the date of the state's first confirmed case of COVID-19. Does not include Florida, New Jersey, or Rhode Island due to lack of data.
+
+**state-data-2020-03-20.csv** - Holds data related to influenza-like illness by state. Data is pulled from https://github.com/reichlab/ncov/tree/master/analyses/ili-data.
+
+**state-data-2020-03-20-ROUNDED.csv** - Same data as *state-data-2020-03-20.csv* except the column **rili_minus** is rounded to three decimal places to build models quicker.
+
+**region-data-2020-03-20.csv** - Holds data related to influenza-like illness by region. Data is pulled from https://github.com/reichlab/ncov/tree/master/analyses/ili-data.
+
+**region-data-2020-03-20-ROUNDED.csv** - Same data as *region-data-2020-03-20.csv* except the column **rili_minus** is rounded to three decimal places to build models quicker.
+
+**states-first-confirmed.csv** - Holds the date of each state's first confirmed case of COVID-19. Source: https://www.nytimes.com/interactive/2020/03/21/us/coronavirus-us-cases-spread.html on 3/22/2020.
+
+**ili-timeseries.R** - The R file to reproduce the forecasts.
+
+**states-and-regions-rili-forecast.RData** - Loads the R workspace for the project.
+
+<h3>Disclaimer</h3>
+
+I do not have domain expertise in anything related to epidemiology. This analysis may very well be tainted by my lack of knowledge in this field. If you have domain knowledge and find something incorrect or misleading, please email me at ryan.kruse9@gmail.com.
+
+
+<h3>Project Details</h3>
+
+The goal of this project is to use the metric **rILI-** to identify states in the United States that are more likely to have had coronavirus before any cases were confirmed there. As described by Reich et. al [here](https://github.com/reichlab/ncov/blob/master/analyses/ili-labtest-report.pdf), **rILI-** is a value that measures the amount of influenza-like illness that are not attributable to influenza. It comes from CDC's Influenza-like Illness Surveillance Network (ILINet), which basically tracks the number of doctors office visits where patients had a fever and at least one other flu-like symptom (such as cough or sore throat) and proceeded to test negative for flu or any other known cause.
+
+A higher **rILI-** indicates more people have flu-like symptoms with an unknown cause. Therefore, a higher **rILI-** may provide evidence for the presence of some other virus, such as the 2019 coronavirus.
+
+This project uses time series analysis and forecasting to predict states' **rILI-** for the weeks from November 17, 2019 until now. Currently, November 17, 2019 is widely believed to be the earliest date the first human could have had COVID-19. The predictions were based on time series data from Week 40 of 2010 until Week 47 of 2019 (which ended on November 17). For each state, we constructed 95% and 99% prediction intervals based on an ETS model using R's **forecast** package. We chose ETS over ARIMA after comparing the two's performance on a train/test split, but we expect both would produce similar forecasted prediction intervals.
+
+![Alabama's rILI- 99% P.I.](images/Alabama-rILI-99.png)
+
+Above, we see the plot for Alabama. The black data line is the weekly observed data before November 17, 2019. The red line is the weekly observed data after November 17, 2019. The gray area to the right shows our forecasted 99% prediction interval. Based on the data prior to November 17, 2019 (and under the same circumstances), we expect 99% of  data points after November 17, 2019 to be in the gray area (our 99% prediction interval). If the observed data points fall outside of our prediction interval, then we have strong evidence the circumstances have changed and something fishy is going on. This does not seem to be the case for Alabama as all the observed data points are comfortably within the prediction intervals.
+
+Maine is a different story...
+
+![Maine's rILI- 99% P.I.](images/Maine-rILI-99.png)
+
+Before jumping to conclusions, it's important to keep in mind that there are **many** potential caveats. For example, it's possible some these people in Maine were seeing a lot about coronavirus on the news and started going to the doctor in December 2019 when they previously wouldn't have. It's also possible that some other non-flu, non-COVID-19 illness swept through Maine in late 2019.
+
+However, Maine's observed data is consistently and significantly outside of the 99% prediction interval, which provides strong evidence that *something* was going on starting in December 2019, two and a half months before the state's first confirmed case of COVID-19. This analysis doesn't prove anything, but it may provide motivation for further research into when COVID-19 actually spread to Maine and other states. It's impossible to know for certain in the absence of more data.
---
+++ COVID19-early-signs/ili-timeseries.R
@@ -0,0 +1,197 @@
+#ili-timeseries.R
+#NOTE: locations with unusable rILI- data: FL, NYC, NJ, RI
+
+# 1) load libarries
+
+library(forecast)
+library(ggplot2)
+library(hash)
+library(lubridate)
+library(tseries)
+library(imputeTS)
+
+#####################################################
+# 2) load data - DATA SOURCE: https://github.com/reichlab/ncov/tree/master/analyses/ili-data (3/21/2020)
+# rounded rili_minus column in effort to speed up model building w/o losing much accuracy (rounded in Excel before loading)
+#NOTE: Need to replace file location for your own computer
+
+# load state data
+dat <- read.table("C:\\Users\\Kruse\\Desktop\\ILI Time Series\\state-data-2020-03-20-ROUNDED.csv",header=TRUE,sep=",",fill=TRUE)
+
+# load region data
+#dat <- read.table("C:\\Users\\Kruse\\Desktop\\ILI Time Series\\region-data-2020-03-20-ROUNDED.csv",header=TRUE,sep=",",fill=TRUE)
+
+#load dates of first confirmed case for each state - DATA SOURCE: https://www.nytimes.com/interactive/2020/03/21/us/coronavirus-us-cases-spread.html on 3/22/2020
+dates <- read.table("C:\\Users\\Kruse\\Desktop\\ILI Time Series\\state-first-confirmed.csv",fileEncoding="UTF-8-BOM",header=TRUE,sep=",",fill=TRUE)
+
+####################################################
+# 3) run functions
+
+#return model for given location based on data before November 17, 2019
+# ... November 17 is widely believed to be the earliest coronavirus could have infected first human in China
+get_location_model <- function(data,loc) {
+  print(loc)
+  dataLoc <- subset(data,data$region == loc, select=c(year, week, rili_minus))
+  dataLoc <- dataLoc[order(dataLoc$year,dataLoc$week),]
+  dataLocbefore = subset(dataLoc, (year<2019 & year>2010) | (year == 2019 & week < 48) | (year==2010 & week>39))
+  freq = 365.25/7
+  tsLoc <- ts(dataLoc['rili_minus'], frequency = freq, start=c(2010,40))
+  tsLocBefore <- ts(dataLocbefore['rili_minus'], frequency = freq, start=c(2010,40))
+  tsLoBefore_clean = na_interpolation(tsLoBefore)
+  tempMod = auto.arima(tsLocBefore)
+  return(tempMod)
+}
+
+get_data <- function(location, data) {
+  result = hash()
+  datLoc <- subset(data,dat$region == location, select=c(year, week, rili_minus))
+  datLoc <- subset(datLoc, (week>39 & year==2010) | (year>2010))
+  datLoc <- datLoc[order(datLoc$year,datLoc$week),]
+  datLocbefore = subset(datLoc, (year<2019 & year>2010) | (year == 2019 & week < 48) | (year==2010 & week>39))
+  freq = 365.25/7
+  tsLoBefore <- ts(datLocbefore['rili_minus'], frequency = freq, start=c(2010,40))
+  tsLoBefore = na_interpolation(tsLoBefore)
+  observedData <- ts(datLoc['rili_minus'], frequency = freq, start=c(2010,40))
+  observedData <- na_interpolation(observedData)
+  result[['before']] = tsLoBefore
+  result[['all']] = observedData
+  return(result)
+}
+
+#plot forecasts and safe to PDF
+forecasts_to_pdf <- function(data, states, fileloc) {
+  path = fileloc
+  pdf(file=path)
+  par(mfrow=c(2,1))
+  for (loc in states) {
+    title = paste(loc, "99% Confidence Forecast of rILI-*")
+    subtitle = "*rILI-: a metric indicating the rate and amount of people seeking\ncare for flu-like symptoms and test negative for flu"
+    f = ets_forecasts_all[[loc]]
+    a = observed_all[[loc]]
+    xax = c(2019,2019.25,2019.5, 2019.75,2020,2020.25)
+    xlabels=c("\nJanuary\n2019","April\n2019","July\n2019","October\n2019","January\n2020","April\n2020")
+    plot(f, xlim=c(2018.9,2020.2),ylim=c(0,max(a)), main=title, sub=subtitle, fcol=NULL,ylab="rILI-",xaxt="none")
+    axis(1, at=xax,labels=xlabels, line=.25, col="white")
+    axis(1,at=xax,labels=FALSE)
+    lines(tail(a,16), col='red',lwd=1)
+    lines(head(a,477), col='black') #run as safety to ensure we are using the correct data
+    legend(2018.9,max(a)*.97, legend=c("Before Nov 17, 2019","After Nov 17, 2019"), col=c("black","red"), lty=c(1,1), cex=.7, bg='lightgray')
+    d = subset(dates, dates$state==loc, select=c(first_case_date))
+    dd = decimal_date(ymd(d[1,1]))
+    d = ymd(d[1,1])
+    abline(v=dd, col="black",lwd=2, lty=c(2))
+    text(dd-.01, max(a)*.89, paste("First\nConfirmed\nCase",format(d, format="%B %d, %Y"),sep="\n"),font=2, cex=.6, adj=c(1))
+  }
+  #save to file
+  dev.off()
+}
+
+compare_arima_ets <- function(trainSet, testSet) {
+  tempmodel = auto.arima(trainSet)
+  f2 = forecast(tempmodel, h=32)
+  print(accuracy(f2,testSet))
+  f3 = forecast(trainSet, h=32)
+  print(accuracy(f3,testSet))
+  results = hash()
+  results[['arima']] = f2
+  results[['ets']] = f3
+  return(results)
+}
+
+#assumes arima_forecasts and ets_forecasts are already defined AND have the same states
+# ... AND are provided as parameters
+see_arima_ets_comparisons <- function(arima, ets, data) {
+  keys = keys(arima)
+  for (key in keys) {
+    print(key)
+    d = get_data(key, data)
+    before = d[['before']]
+    testSet = tail(before,32)
+    print(accuracy(arima[[key]], testSet))
+    print(accuracy(ets[[key]], testSet))
+  }
+}
+
+######################################################
+# 4) build forecasts and write to PDF
+#NOTE: change path variable for your own computer
+
+#make list of all locations with data
+states = unique(dat$region)
+removeList = c("Florida", "New York City","New Jersey","Rhode Island")
+index = 1
+for (s in states) {
+  if (s %in% removeList) {
+    #states[index] <- NULL
+    states = states[-index]
+  }
+  else{
+    index = index + 1
+  }
+}
+
+ets_forecasts_all = hash()
+observed_all = hash()
+for (state in states) {
+  #let test set be the last 48 points of 'before' set
+  print(state)
+  d = get_data(state, dat)
+  before = d[['before']]
+  temp_all = d[['all']]
+  temp_ets = forecast(before, h=16, level=c(99)) #forecast the 16 weeks after Nov 17, 2019 - use 99% prediction interval
+  ets_forecasts_all[[state]] = temp_ets #add forecasts to hash to allow additional plotting, if desired
+  observed_all[[state]] = temp_all #add all observed data to hash to allow additional plotting, if desired
+}
+
+#save to PDF (change path)
+path = "C:\\Users\\Kruse\\Desktop\\ILI Time Series\\state-forecasts.pdf"
+forecasts_to_pdf(dat,states, path)
+
+######################################################
+#code for testing auto.arima vs ETS
+#conclusion: some states are forecasted better using different methods, prediction intervals are similar
+  # ... MAPE/MASE were generally better for ETS, so we move forward with ETS
+
+d = get_data("California", dat)
+before = d[['before']]
+trainSet = head(before,429)
+testSet = tail(before,48)
+allData = d[['all']]
+lines(allData,col='red')
+
+#pick 12 random states to compare forecasting methods
+arima_forecasts = hash()
+ets_forecasts = hash()
+for (state in c("Minnesota","California","Delaware","Colorado","Iowa","Georgia","South Dakota",
+                "Pennsylvania","Tennessee","Maryland","Hawaii","Alabama")) {
+  #let test set be the last 48 points of 'before' set
+  print(state)
+  d = get_data(state, dat)
+  before = d[['before']]
+  trainSet = head(before,445)
+  testSet = tail(before,32)
+  allData = d[['all']]
+  results = compare_arima_ets(trainSet,testSet)
+  arima_forecasts[[state]] = results[['arima']]
+  ets_forecasts[[state]] = results[['ets']]
+}
+
+#plot comparison
+  #arima better: MN, CA, PE
+  #ets better: CO, IA, GA, SD, TE
+  #about same: DE (both bad but PI are fine), MD (both bad but PI are fine), HI (both bad but PI fine), AL (both good)
+#MAPE/MASE comparison
+  #arima better: MN, PE, CA, AL
+  #ets better: SD, TE, MD (bad), IA, HI, GA, DE, CO
+
+#see plots of arima and ETS
+loc = "Alabama"
+par(mfrow=c(1,1))
+d = get_data(loc, dat)
+allData = head(d[['all']],477)
+plot(arima_forecasts[[loc]])
+lines(allData,col='red')
+plot(ets_forecasts[[loc]])
+lines(allData,col='red')
+
+#######################################################
